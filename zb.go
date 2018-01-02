632c9ca4c853b65515b2b1ef9a0baedec8fe63b4
huangwenwei <mr.huangwenwei@gmail.com>
2018-01-02 12:01:22 +0800

    add accountInfo api

--- zb.go
+++ zb.go
@@ -2,44 +2,89 @@ package main
 
 import (
 	"encoding/json"
+	"strconv"
+	"time"
 
 	"github.com/go-resty/resty"
 	"github.com/mitchellh/mapstructure"
 )
 
-type order []float64
-
-// RespDepth 深度接口返回结构体
-type RespDepth struct {
-	Timestamp float64 `mapstructure:"timestamp"`
-	Asks      []order `mapstructure:"asks"`
-	Bids      []order `mapstructure:"bids"`
+//==================================================//
+type depthOrder []float64
+type respDepth struct {
+	Timestamp float64      `mapstructure:"timestamp"`
+	Asks      []depthOrder `mapstructure:"asks"`
+	Bids      []depthOrder `mapstructure:"bids"`
 }
 
+//==================================================//
+type accountInfoCoin struct {
+	EnName        string `mapstructure:"enName"`
+	Freez         string `mapstructure:"freez"`
+	UnitDecimal   string `mapstructure:"unitDecimal"`
+	CnName        string `mapstructure:"cnName"`
+	IsCanRecharge bool   `mapstructure:"isCanRecharge"`
+	UnitTag       string `mapstructure:"unitTag"`
+	IsCanWithdraw bool   `mapstructure:"isCanWithdraw"`
+	Available     string `mapstructure:"available"`
+	Key           string `mapstructure:"key"`
+}
+type accountInfoBase struct {
+	Username             string `mapstructure:"username"`
+	TradePasswordEnabled bool   `mapstructure:"trade_password_enabled"`
+	AuthGoogleEnabled    bool   `mapstructure:"auth_google_enabled"`
+	AuthMobileEnabled    bool   `mapstructure:"auth_mobile_enabled"`
+}
+type accountInfoResult struct {
+	Coins    []accountInfoCoin `mapstructure:"coins"`
+	BaseInfo accountInfoBase   `mapstructure:"base"`
+}
+type respAccountInfo struct {
+	Result accountInfoResult `mapstructure:"result"`
+}
+
+//==================================================//
 var (
 	apiClient, tradeClient *resty.Client
+	mapInterface           map[string]interface{}
 )
 
 func init() {
-	apiClient = resty.New().SetDebug(true).
+	apiClient = resty.New().SetDebug(false).
 		SetHostURL("http://api.zb.com/data/v1/").
 		SetHeaders(map[string]string{
 			"Content-Type": "application/json",
-			"User-Agent":   "https://huangwenwei.com",
+		})
+
+	tradeClient = resty.New().SetDebug(false).
+		SetHostURL("https://trade.zb.com/api/").
+		SetHeaders(map[string]string{
+			"Content-Type": "application/json",
 		})
 }
 
-func depth(api string, market string, size string) *RespDepth {
+func depth(api string, market string, size string) *respDepth {
 	resp, _ := apiClient.SetQueryParams(map[string]string{
 		"market": market,
 		"size":   size,
 	}).R().Get(api)
 
-	var (
-		res RespDepth
-		f   interface{}
-	)
-	json.Unmarshal(resp.Body(), &f)
-	mapstructure.Decode(f.(map[string]interface{}), &res)
+	var res respDepth
+	json.Unmarshal(resp.Body(), &mapInterface)
+	mapstructure.Decode(mapInterface, &res)
+	return &res
+}
+
+func accountInfo(api string, sign string) *respAccountInfo {
+	resp, _ := tradeClient.SetQueryParams(map[string]string{
+		"accesskey": config.accesstoken,
+		"method":    "getAccountInfo",
+		"sign":      sign,
+		"reqTime":   strconv.FormatInt(time.Now().UnixNano()/1000000, 10),
+	}).R().Get(api)
+
+	var res respAccountInfo
+	json.Unmarshal(resp.Body(), &mapInterface)
+	mapstructure.Decode(mapInterface, &res)
 	return &res
 }
