33c07bd68e1a970255640bc0d2b55afa72b8bafa
huangwenwei <mr.huangwenwei@gmail.com>
2018-01-27 17:34:00 +0800

    add zb api usage

--- main.go
+++ main.go
@@ -1,62 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	createOrderParams := map[string]string{
-		"accesskey": config.accesstoken,
-		"amount":    "1",
-		"currency":  "bts_usdt",
-		"price":     "20",
-		"tradeType": "0",
-		"method":    "order",
-	}
-
-	createOrderSorted := sortParams(createOrderParams)
-	createOrderSign := hmacSign(createOrderSorted)
-	crea := createOrder("order", "1", "bts_usdt", "0", "20", createOrderSign)
-	fmt.Println(crea)
-
-	orderParams := map[string]string{
-		"accesskey": config.accesstoken,
-		"currency":  "bts_usdt",
-		"method":    "getOrdersNew",
-		"pageIndex": "1",
-		"pageSize":  "50",
-		"tradeType": "0",
-	}
-	orderSorted := sortParams(orderParams)
-	orderSign := hmacSign(orderSorted)
-	resss := getOrders("getOrdersNew", "bts_usdt", "0", orderSign)
-	fmt.Println(resss)
-
-	id := (*resss)[0].ID
-	cancelParams := map[string]string{
-		"accesskey": config.accesstoken,
-		"currency":  "bts_usdt",
-		"id":        id,
-		"method":    "cancelOrder",
-	}
-	cancelSorted := sortParams(cancelParams)
-	cancelSign := hmacSign(cancelSorted)
-	resp := cancelOrder("cancelOrder", id, "bts_usdt", cancelSign)
-	fmt.Println(resp)
-
-	trades("trades", "btc_usdt")
-	kline("kline", "btc_usdt", "1min", "10")
-	tick := getTicker("ticker", "btc_usdt")
-	fmt.Println(tick.Ticker.Last)
-
 	r := newReceiver()
 	cmdExecute(r)
-
-	params := map[string]string{
-		"accesskey": config.accesstoken,
-		"method":    "getAccountInfo",
-	}
-	sorted := sortParams(params)
-	sign := hmacSign(sorted)
-	accountInfo("getAccountInfo", sign)
-
 	r.Wait()
 }
--- zb.go
+++ zb.go
@@ -6,6 +6,8 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+// 市场深度
+// depth("depth", "btc_usdt", "20")
 func depth(api, market, size string) *respDepth {
 	resp, _ := dataClient.SetQueryParams(map[string]string{
 		"market": market,
@@ -21,6 +23,8 @@ func depth(api, market, size string) *respDepth {
 	return &res
 }
 
+// 行情
+// getTicker("ticker", "btc_usdt")
 func getTicker(api, market string) *respTicker {
 	resp, _ := dataClient.SetQueryParams(map[string]string{
 		"market": market,
@@ -29,6 +33,9 @@ func getTicker(api, market string) *respTicker {
 	json.Unmarshal(resp.Body(), &res)
 	return &res
 }
+
+// K线
+// kline("kline", "btc_usdt", "1min", "10")
 func kline(api, market, timeType, size string) *respKline {
 	resp, _ := dataClient.SetQueryParams(map[string]string{
 		"market": market,
@@ -44,6 +51,8 @@ func kline(api, market, timeType, size string) *respKline {
 	return &res
 }
 
+// 历史成交
+// trades("trades", "btc_usdt")
 func trades(api, market string) *respTrades {
 	resp, _ := dataClient.SetQueryParams(map[string]string{
 		"market": market,
@@ -53,6 +62,14 @@ func trades(api, market string) *respTrades {
 	return &res
 }
 
+// 获取用户信息
+// params := map[string]string{
+// 	"accesskey": config.accesstoken,
+// 	"method":    "getAccountInfo",
+// }
+// sorted := sortParams(params)
+// sign := hmacSign(sorted)
+// accountInfo("getAccountInfo", sign)
 func accountInfo(api, sign string) *respAccountInfo {
 	resp, _ := tradeClient.SetQueryParams(map[string]string{
 		"method": "getAccountInfo",
@@ -67,6 +84,18 @@ func accountInfo(api, sign string) *respAccountInfo {
 	return &res
 }
 
+// 委托下单
+// createOrderParams := map[string]string{
+// 	"accesskey": config.accesstoken,
+// 	"amount":    "1",
+// 	"currency":  "bts_usdt",
+// 	"price":     "20",
+// 	"tradeType": "0",
+// 	"method":    "order",
+// }
+// createOrderSorted := sortParams(createOrderParams)
+// createOrderSign := hmacSign(createOrderSorted)
+// createOrder("order", "1", "bts_usdt", "0", "20", createOrderSign)
 func createOrder(api, amount, currency, tradeType, price, sign string) bool {
 	resp, _ := tradeClient.SetQueryParams(map[string]string{
 		"amount":    amount,
@@ -79,7 +108,20 @@ func createOrder(api, amount, currency, tradeType, price, sign string) bool {
 	return simpleResponse(resp)
 }
 
+// 获取委托买单或卖单
 // tradeType 交易类型1/0[buy/sell]
+
+// orderParams := map[string]string{
+// 	"accesskey": config.accesstoken,
+// 	"currency":  "bts_usdt",
+// 	"method":    "getOrdersNew",
+// 	"pageIndex": "1",
+// 	"pageSize":  "50",
+// 	"tradeType": "0",
+// }
+// orderSorted := sortParams(orderParams)
+// orderSign := hmacSign(orderSorted)
+// getOrders("getOrdersNew", "bts_usdt", "0", orderSign)
 func getOrders(api, currency, tradeType, sign string) *respOrders {
 	resp, _ := tradeClient.SetQueryParams(map[string]string{
 		"currency":  currency,
@@ -94,6 +136,17 @@ func getOrders(api, currency, tradeType, sign string) *respOrders {
 	return &res
 }
 
+// 取消委托
+// id := (*resss)[0].ID
+// cancelParams := map[string]string{
+// 	"accesskey": config.accesstoken,
+// 	"currency":  "bts_usdt",
+// 	"id":        id,
+// 	"method":    "cancelOrder",
+// }
+// cancelSorted := sortParams(cancelParams)
+// cancelSign := hmacSign(cancelSorted)
+// cancelOrder("cancelOrder", id, "bts_usdt", cancelSign)
 func cancelOrder(api, id, currency, sign string) bool {
 	resp, _ := tradeClient.SetQueryParams(map[string]string{
 		"currency": currency,
